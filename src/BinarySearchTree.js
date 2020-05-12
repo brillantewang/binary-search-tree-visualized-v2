86a8e5649d3a9ac6caf9cd071800c8588ab9eaaf
Brillante Wang <brillante.wang@gmail.com>
2020-05-12 00:19:39 -0500

    Center the tree
    Used https://codesandbox.io/s/vvz51w5n63 as a resource.

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Tree from 'react-d3-tree';
+import styled from 'styled-components';
+
+const TreeContainer = styled.div`
+  width: 100vw;
+`;
 
 const TreeData = [
   {
@@ -16,7 +21,29 @@ const TreeData = [
 ];
 
 const BinarySearchTree = () => {
-  return <Tree data={TreeData} />
+  const treeContainerEl = useRef(null);
+  const [translate, setTranslate] = useState(null);
+
+  useEffect(() => {
+    // center the tree
+    const dimensions = treeContainerEl.current.getBoundingClientRect();
+    setTranslate({
+      x: dimensions.width / 2,
+      y: dimensions.height / 2
+    });
+  }, []);
+
+  return (
+    <TreeContainer ref={treeContainerEl}>
+      {translate &&
+        <Tree
+          data={TreeData}
+          orientation='vertical'
+          translate={translate}
+        />
+      }
+    </TreeContainer>
+  )
 };
 
 export default BinarySearchTree;
