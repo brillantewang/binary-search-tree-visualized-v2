e23eff142db9300545de9f27bcd73dfbaee756e9
Brillante Wang <brillante.wang@gmail.com>
2020-05-16 15:33:31 -0500

    Hide paths to blank nodes

--- src/StyledTree.js
+++ src/StyledTree.js
@@ -6,21 +6,36 @@ const TreeContainer = styled.div`
   height: 100vh;
 `;
 
-const TreeDefaultData = [
+const buildBlankNode = () => ({
+  name: '',
+  nodeSvgShape: {
+    shape: 'circle',
+    shapeProps: {
+      r: 0,
+    }
+  }
+})
+
+const treeDefaultData = [
   {
     name: 'whoa',
     children: [
       {
         name: 'okay',
         children: [
+          buildBlankNode(),
           {
             name: 'haha',
+            children: [
+              buildBlankNode(),
+              {
+                name: 'huh',
+              },
+            ]
           },
         ]
       },
-      {
-        name: 'alright',
-      }
+      buildBlankNode()
     ]
   }
 ];
@@ -55,32 +70,60 @@ const nodeSvgShape = {
 
 const textLayout = {
   textAnchor: 'middle',
+  x: 0,
+  y: 0
 };
 
 const StyledTree = () => {
   const treeContainerEl = useRef(null);
   const [translate, setTranslate] = useState(null);
 
-  useEffect(() => {
-    // center the tree
+  const getPathsToBlankNodes = () => {
+    const blankCircles = document.querySelectorAll('circle[r="0"]');
+    const blankNodeIds = [...blankCircles].map(circle => circle.parentNode.id);
+
+    return blankNodeIds.map(id => document.querySelector(`path[data-target-id="${id}"`))
+  };
+
+  const hidePathsToBlankNodes = () => {
+    // We can have blank nodes in the tree to act as sibling placeholders.
+    // That allows single child nodes to be offset from their parents instead of
+    // directly below (the default behavior for react-d3-tree). But we need to hide
+    // the paths to these blank nodes.
+    const pathsToBlankNodes = getPathsToBlankNodes();
+    pathsToBlankNodes.forEach(path => path.style.strokeWidth = "0");
+  };
+  
+  const centerTree = () => {
     const dimensions = treeContainerEl.current.getBoundingClientRect();
     setTranslate({
       x: dimensions.width / 2,
       y: dimensions.height / 2
     });
+  }
+
+  useEffect(() => {
+    // https://medium.com/javascript-in-plain-english/how-to-use-async-function-in-react-hook-useeffect-typescript-js-6204a788a435
+    async function setupTree() {
+      await centerTree();
+      hidePathsToBlankNodes();
+    };
+
+    setupTree();
   }, []);
 
   return (
     <TreeContainer ref={treeContainerEl}>
       {translate &&
         <Tree
-          data={TreeDefaultData}
+          data={treeDefaultData}
           orientation='vertical'
           pathFunc='straight'
           translate={translate}
           styles={styles}
           nodeSvgShape={nodeSvgShape}
           textLayout={textLayout}
+          onUpdate={hidePathsToBlankNodes}
         />
       }
     </TreeContainer>
