b0007098d88571888645be0d23c97876fc877da8
Brillante Wang <brillante.wang@gmail.com>
2020-05-16 18:00:58 -0500

    Automatically insert blank placeholder nodes

--- src/StyledTree.js
+++ src/StyledTree.js
@@ -1,44 +1,33 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Tree from 'react-d3-tree';
 import styled from 'styled-components/macro';
+import cloneDeep from 'clone-deep';
 
 const TreeContainer = styled.div`
   height: 100vh;
 `;
 
-const buildBlankNode = () => ({
-  name: '',
-  nodeSvgShape: {
-    shape: 'circle',
-    shapeProps: {
-      r: 0,
-    }
-  }
-})
-
-const treeDefaultData = [
-  {
-    name: 'whoa',
-    children: [
-      {
-        name: 'okay',
-        children: [
-          buildBlankNode(),
-          {
-            name: 'haha',
-            children: [
-              buildBlankNode(),
-              {
-                name: 'huh',
-              },
-            ]
-          },
-        ]
-      },
-      buildBlankNode()
-    ]
-  }
-];
+const treeDefaultData = {
+  name: '12',
+  children: [
+    {
+      name: '8',
+      children: [
+        {
+          name: '15',
+          children: [
+            {
+              name: '3',
+            },
+          ]
+        },
+        {
+          name: '19',
+        },
+      ]
+    },
+  ]
+};
 
 const nodeStyle = {
   circle: {
@@ -76,20 +65,51 @@ const textLayout = {
 
 const StyledTree = () => {
   const treeContainerEl = useRef(null);
-  const [translate, setTranslate] = useState(null);
+  const [translate, setTranslate] = useState({ x: 0, y: 0 });
+  const [treeData, setTreeData] = useState(treeDefaultData);
+
+  const buildBlankNode = () => ({
+    name: '',
+    nodeSvgShape: {
+      shape: 'circle',
+      shapeProps: {
+        r: 0,
+      }
+    }
+  })
+
+  const insertBlankChildNode = (node) => {
+    const childNode = node.children[0];
+    const blankNode = buildBlankNode();
+    if (parseInt(childNode.name) < parseInt(node.name)) {
+      node.children.push(blankNode);
+    } else {
+      node.children.unshift(blankNode);
+    }
+  }
+
+  const insertBlankChildrenNodesRecursively = node => {
+    const children = node.children || [];
+    children.forEach(child => insertBlankChildrenNodesRecursively(child));
+    if (children.length === 1) insertBlankChildNode(node);
+  }
+
+  const insertBlankPlaceholderNodes = () => {
+    // We insert blank nodes in the tree to act as sibling placeholders.
+    // That allows single child nodes to be offset from their parents instead of
+    // directly below (the default behavior for react-d3-tree).
+    const treeDataCopy = cloneDeep(treeData);
+    insertBlankChildrenNodesRecursively(treeDataCopy);
+    setTreeData(treeDataCopy);
+  };
 
   const getPathsToBlankNodes = () => {
     const blankCircles = document.querySelectorAll('circle[r="0"]');
     const blankNodeIds = [...blankCircles].map(circle => circle.parentNode.id);
-
     return blankNodeIds.map(id => document.querySelector(`path[data-target-id="${id}"`))
   };
 
   const hidePathsToBlankNodes = () => {
-    // We can have blank nodes in the tree to act as sibling placeholders.
-    // That allows single child nodes to be offset from their parents instead of
-    // directly below (the default behavior for react-d3-tree). But we need to hide
-    // the paths to these blank nodes.
     const pathsToBlankNodes = getPathsToBlankNodes();
     pathsToBlankNodes.forEach(path => path.style.strokeWidth = "0");
   };
@@ -106,6 +126,7 @@ const StyledTree = () => {
     // https://medium.com/javascript-in-plain-english/how-to-use-async-function-in-react-hook-useeffect-typescript-js-6204a788a435
     async function setupTree() {
       await centerTree();
+      insertBlankPlaceholderNodes();
       hidePathsToBlankNodes();
     };
 
@@ -114,18 +135,16 @@ const StyledTree = () => {
 
   return (
     <TreeContainer ref={treeContainerEl}>
-      {translate &&
-        <Tree
-          data={treeDefaultData}
-          orientation='vertical'
-          pathFunc='straight'
-          translate={translate}
-          styles={styles}
-          nodeSvgShape={nodeSvgShape}
-          textLayout={textLayout}
-          onUpdate={hidePathsToBlankNodes}
-        />
-      }
+      <Tree
+        data={treeData}
+        orientation='vertical'
+        pathFunc='straight'
+        translate={translate}
+        styles={styles}
+        nodeSvgShape={nodeSvgShape}
+        textLayout={textLayout}
+        onUpdate={hidePathsToBlankNodes}
+      />
     </TreeContainer>
   )
 };
